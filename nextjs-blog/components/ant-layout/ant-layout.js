43eac717154a0244343196d0de01f64908a34419
Dam6ien <37600295+Dam6ien@users.noreply.github.com>
2021-10-28 20:04:28 +0200

    Update ant-layout.js

--- nextjs-blog/components/ant-layout/ant-layout.js
+++ nextjs-blog/components/ant-layout/ant-layout.js
@@ -28,26 +28,25 @@ class SiderDemo extends React.Component {
         <Sider collapsible collapsed={collapsed} onCollapse={this.onCollapse}>
           <div className="logo" />
           <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
-            <Menu.Item key="1" icon={<PieChartOutlined />}>
-              Option 1
-            </Menu.Item>
-            <Menu.Item key="2" icon={<DesktopOutlined />}>
-              Option 2
-            </Menu.Item>
-            <SubMenu key="sub1" icon={<UserOutlined />} title="User">
-              <Menu.Item key="3">Tom</Menu.Item>
+            <SubMenu key="sub1" icon={<UserOutlined />} title="Sub family">
+              <Menu.Item key="3">Myrmicinae</Menu.Item>
               <Menu.Item key="4">Bill</Menu.Item>
               <Menu.Item key="5">Alex</Menu.Item>
             </SubMenu>
-            <SubMenu key="sub2" icon={<TeamOutlined />} title="Team">
-              <Menu.Item key="6">Team 1</Menu.Item>
+            <SubMenu key="sub2" icon={<TeamOutlined />} title="Genus">
+              <Menu.Item key="6">Tetramorium</Menu.Item>
               <Menu.Item key="8">Team 2</Menu.Item>
+            </SubMenu>
+            <SubMenu key="sub3" icon={<TeamOutlined />} title="Species">
+              <Menu.Item key="7">Eminii</Menu.Item>
+              <Menu.Item key="9">Team 2</Menu.Item>
             </SubMenu>
             <Menu.Item key="9" icon={<FileOutlined />}>
               Files
             </Menu.Item>
           </Menu>
         </Sider>
+        
         <Layout className="site-layout">
           <Header className="site-layout-background" style={{ padding: 0 }} />
           <Content style={{ margin: '0 16px' }}>
